f549b2f786e07f5e9f0d92f1903f8b1fbe616f19
Donna Amos <donnacamos.developer@gmail.com>
2019-07-15 20:41:38 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
-Artist < ActiveRecord::Base 
-  include Slugifiable::InstanceMethods 
+class Artist < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
 
-  has_many :songs 
-  has_many :genres, through: :songs  
-  end 
+  has_many :songs
+  has_many :genres, through: :songs
+end
