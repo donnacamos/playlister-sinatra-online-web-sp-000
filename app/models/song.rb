984859179379849d86b4b3b3117967abd0502d81
Donna Amos <donnacamos.developer@gmail.com>
2019-07-15 19:34:37 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,8 @@ Song < ActiveRecord::Base
    include Slugifiable::InstanceMethods 
    
 belongs_to :artist 
-
-has_many :genres, through: :songs 
 has_many :song_genres 
+has_many :genres, through: :song_genres 
+ 
 
 end 
